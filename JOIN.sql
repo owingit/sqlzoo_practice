44f5360d39f4f39d4dad1c6cefaf959cbecea96c
Owen Martin <martin.owen22@gmail.com>
2018-07-25 11:44:04 -0400

    Update JOIN.sql

--- JOIN.sql
+++ JOIN.sql
@@ -2,9 +2,8 @@
  * Section 6. JOIN operations
  * https://sqlzoo.net/wiki/The_JOIN_operation
  */
- 
---#1
-/*
+
+/* #1
  * Problem: Show matchid and player name for all goals scored by Germany.
  */
 
@@ -12,8 +11,7 @@ SELECT matchid, player
   FROM goal 
  WHERE teamid LIKE 'GER';
  
---#2
- /*
+ /* #2
   * Problem: Show id, stadium, team1, team2 for just game 1012
   */
  
@@ -21,16 +19,15 @@ SELECT id, stadium, team1, team2
   FROM game
  WHERE id = 1012;
  
---#3
- /*
+ /* #3
   * Show the player, teamid, stadium and mdate for every German goal.
   */
 SELECT player, teamid, stadium, mdate
   FROM game JOIN goal ON (game.id = goal.matchid)
  WHERE teamid = 'GER';
  
---#4
- /*
+
+ /* #4
   * Problem: Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
   */
   
@@ -38,8 +35,7 @@ SELECT team1, team2, player
   FROM game JOIN goal ON (game.id = goal.matchid)
  WHERE player LIKE 'Mario%';
 
---#5
- /*
+ /* #5
   * Problem: Show player, teamid, coach, gtime for all goals scored in the first 10 minutes 
   */
   
@@ -47,8 +43,7 @@ SELECT player, teamid, coach, gtime
   FROM goal JOIN eteam ON (goal.teamid = eteam.id)
  WHERE gtime<=10;
  
- --#6
- /*
+ /* #6
   * Problem: List the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
   */
   
@@ -56,8 +51,7 @@ SELECT mdate, teamname
   FROM game JOIN eteam ON (game.team1 = eteam.id)
  WHERE coach = 'Fernando Santos';
  
---#7
- /*
+ /* #7
   * Problem: List the player for every goal scored in a game where the stadium was 'National Stadium, Warsaw'
   */
   
@@ -65,8 +59,7 @@ SELECT player
   FROM goal JOIN game ON (goal.matchid = game.id)
  WHERE stadium = 'National Stadium, Warsaw';
  
---#8 
- /*
+ /* #8 
   * Problem: Show the name of all players who scored a goal against Germany.
   */
   
@@ -74,8 +67,7 @@ SELECT DISTINCT player
   FROM game JOIN goal ON (goal.matchid = game.id) 
  WHERE (team1 = 'GER' OR team2 = 'GER') AND teamid != 'GER';
  
- --#9
- /*
+ /* #9
   * Problem: Show teamname and the total number of goals scored.
   */
   
@@ -83,8 +75,7 @@ SELECT teamname, COUNT(*)
   FROM eteam JOIN goal ON (eteam.id = goal.teamid)
  GROUP BY teamname;
  
-  --#10
- /*
+ /* #10
   * Problem: Show the stadium and the number of goals scored in each stadium.
   */
  
@@ -92,8 +83,8 @@ SELECT teamname, COUNT(*)
   FROM game JOIN goal ON (goal.matchid = game.id)
  GROUP BY stadium;
  
-   --#11
- /*
+
+ /* #11
   * Problem: For every match involving 'POL', show the matchid, date and the number of goals scored.
   */
   
@@ -103,8 +94,8 @@ SELECT matchid, mdate, COUNT(teamid)
  GROUP BY matchid, mdate;
  
  
-    --#12
- /*
+
+ /* #12
   * Problem:  For every match where 'GER' scored, show matchid, match date and the number of goals scored by 'GER'
   */
 
@@ -113,8 +104,8 @@ SELECT matchid, mdate, COUNT(teamid)
  WHERE (team1 = 'GER' OR team2 = 'GER') AND teamid = 'GER'
 GROUP BY matchid, mdate;
 
---#13
-/*
+
+/* #13
  * Problem: List every match with the goals scored by each team as shown. Sort your result by mdate, matchid, team1 and team2.
  */
 SELECT mdate, team1,
