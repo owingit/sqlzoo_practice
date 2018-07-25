7c55c65711a806aaf8942bf7446fccc24bda4eb2
Owen Martin <martin.owen22@gmail.com>
2018-07-25 13:01:35 -0400

    Create USING_NULL.sql

---
+++ USING_NULL.sql
@@ -0,0 +1,81 @@
+/* 
+ * Section 8. Working with NULL
+ * https://sqlzoo.net/wiki/Using_Null
+ */
+ 
+/* #1 
+ * Problem: List the teachers who have NULL for their department.
+ */
+ 
+SELECT name 
+  FROM teacher
+ WHERE dept IS NULL;
+
+/* #2 
+ * Problem: Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+ */
+
+SELECT teacher.name, dept.name
+FROM teacher INNER JOIN dept ON (teacher.dept = dept.id);
+
+/* #3
+ * Problem: Use a different join to see all the teachers
+ */
+
+SELECT teacher.name, dept.name
+FROM teacher LEFT JOIN dept ON (teacher.dept = dept.id);
+
+/* #4
+ * Problem: Use a different JOIN so that all departments are listed.
+ */
+ 
+SELECT teacher.name, dept.name 
+FROM teacher RIGHT JOIN dept ON (teacher.dept = dept.id);
+
+/* #5
+ * Problem: Show teacher name and mobile number or '07986 444 2266'
+ */ 
+
+SELECT name, COALESCE(mobile, '07986 444 2266') FROM teacher;
+
+/* #6
+ * Problem: Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+ */
+ 
+SELECT COALESCE(teacher.name, 'NONE'), COALESCE(dept.name, 'None')
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id);
+
+/* #7
+ * Problem: Use COUNT to show the number of teachers and the number of mobile phones.
+ */
+
+SELECT COUNT(teacher.id), COUNT(mobile) FROM teacher;
+
+/* #8
+ * Problem: Use COUNT and GROUP BY dept.name to show each department and the number of staff.
+ */
+ 
+SELECT dept.name, COUNT(teacher.id)
+  FROM teacher RIGHT JOIN dept ON (teacher.dept=dept.id)
+GROUP BY dept.name;
+
+/* #9
+ * Problem: Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+ */
+ 
+SELECT teacher.name, 
+CASE WHEN dept.id = 1 THEN 'Sci' 
+     WHEN dept.id = 2 THEN 'Sci' 
+     ELSE 'Art' END
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id);
+
+/* #10
+ * Problem: Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+ */
+ 
+SELECT teacher.name, 
+CASE WHEN dept.id = 1 THEN 'Sci' 
+     WHEN dept.id = 2 THEN 'Sci' 
+     WHEN dept.id = 3 THEN 'Art'
+     ELSE 'None' END
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id);
